5f0882e804f9dcfa08e8f4d9d7a39f21487ca22e
konekotech <89322889+konekotech@users.noreply.github.com>
2024-05-03 20:21:11 +0900

    QRコード読めた

---
+++ src/app/providers.tsx
@@ -0,0 +1,9 @@
+// app/providers.tsx
+"use client";
+
+import { FC, PropsWithChildren } from "react";
+import { ThemeProvider } from "next-themes";
+
+export const Providers: FC<PropsWithChildren> = ({ children }) => {
+    return <ThemeProvider attribute="class">{children}</ThemeProvider>;
+};
